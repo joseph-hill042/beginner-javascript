4b8a81d3ffe257873b433fa07088c121eaa0cea3
Joseph Hill <joseph.hill1@outlook.com>
2020-12-16 18:14:32 -0500

    finished Number type section

---
+++ playground/types.js
@@ -0,0 +1,15 @@
+// Strings
+const firstName = 'Joseph';
+const middleName = 'Anthony';
+const lastName = `Hill`;
+
+console.info(`Hi, ${firstName} ${middleName} ${lastName}`);
+
+// Numbers
+
+const a = 10;
+const b = 20;
+
+const result = a + b;
+
+console.info(`${a} + ${b} = ${result}`);
