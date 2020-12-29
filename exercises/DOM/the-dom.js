99f1ba1efd335f4657f9d5c5861f151d40478b7b
Joseph Hill <joseph.hill1@outlook.com>
2020-12-28 20:36:25 -0500

    selecting elements section

---
+++ exercises/DOM/the-dom.js
@@ -0,0 +1,4 @@
+const p = document.querySelector('p');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
