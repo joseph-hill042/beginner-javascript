e9dcd200c0699fae62aa85597fb2c936eb1b57c6
Joseph Hill <joseph.hill1@outlook.com>
2020-12-30 21:13:31 -0500

    element properties and methods section

--- exercises/DOM/the-dom.js
+++ exercises/DOM/the-dom.js
@@ -3,4 +3,13 @@ const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
 const heading = document.querySelector('h2');
-console.dir(heading);
+
+console.info(heading.textContent);
+console.info(heading.innerText);
+// set the textContent property on that element
+heading.textContent = 'Joseph is cool';
+console.info(heading.textContent);
+console.info(heading.innerText);
+
+console.info(heading.innerHTML);
+console.info(heading.outerHTML);
