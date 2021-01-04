5bbdafa123c0e9334ee1cefa4e20205c61e2c9a7
Joseph Hill <joseph.hill1@outlook.com>
2021-01-04 18:47:17 -0500

    element properties and methods

--- exercises/DOM/the-dom.js
+++ exercises/DOM/the-dom.js
@@ -4,12 +4,18 @@ const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
 const heading = document.querySelector('h2');
 
-console.info(heading.textContent);
-console.info(heading.innerText);
+console.info('textContent:::', heading.textContent);
+console.info('innerText:::', heading.innerText);
 // set the textContent property on that element
 heading.textContent = 'Joseph is cool';
-console.info(heading.textContent);
-console.info(heading.innerText);
+console.info('textContent:::', heading.textContent);
+console.info('innerText:::', heading.innerText);
 
-console.info(heading.innerHTML);
-console.info(heading.outerHTML);
+console.info('innerHTML:::', heading.innerHTML);
+console.info('outerHTML:::', heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+console.info(pizzaList.textContent);
+
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+pizzaList.insertAdjacentText('beforeend', '🍕');
