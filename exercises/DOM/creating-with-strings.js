2c0862ed5c9c909a2af09794890204e7980360c5
Joseph Hill <joseph.hill1@outlook.com>
2021-01-05 22:23:52 -0500

    html from strings and xss

---
+++ exercises/DOM/creating-with-strings.js
@@ -0,0 +1,19 @@
+console.info('it works!');
+const item = document.querySelector('.item');
+const src = `https://picsum.photos/200`;
+const desc = `Cute Pup <img src="https://picsum.photos/50" onload="alert('HACKED')" >`;
+const myHtml = `
+<div class="wrapper">
+  <h2>${desc}</h2>
+  <img src="${src}" alt="${desc}">
+</div>
+`;
+ 
+// turn a string into an element
+const myFragment = document
+                    .createRange()
+                    .createContextualFragment(myHtml);
+
+console.log(myFragment);
+
+document.body.appendChild(myFragment);
