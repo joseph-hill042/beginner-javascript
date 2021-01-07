85c117393cd874040ad3fede5b14cec4cd9ad7e7
Joseph Hill <joseph.hill1@outlook.com>
2021-01-07 18:06:29 -0500

    dom cardio section

--- exercises/DOM/DOM-cardio.js
+++ exercises/DOM/DOM-cardio.js
@@ -30,3 +30,22 @@ const myDivFragment = document.createRange().createContextualFragment(myHtml);
 wrapper.prepend(myDivFragment);
 wrapper.firstElementChild.children[1].classList.add('warning');
 wrapper.firstElementChild.children[0].remove();
+
+function generatePlayerCard(name, age, height) {
+  const ageInDogYears = age * 7;
+  return `
+    <div class="playerCard">
+      <h2>${name.toUpperCase()} - ${age}</h2>
+      <p>They are ${height} and ${age} years old. In Dog years this person would be ${ageInDogYears}. That would be a tall dog!</p>
+    </div>
+  `;
+}
+
+const cardDiv = `
+  <div class="cards"></div>
+`;
+const cardDivFragment = document
+  .createRange()
+  .createContextualFragment(cardDiv);
+
+const card1 = generatePlayerCard('joseph', 41, '6 ft');
